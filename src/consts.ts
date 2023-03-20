349c358fa17f35a4b97812937b932191ed6c5db6
WillYallop <hello@williamyallop.com>
2023-03-20 22:36:16 +0000

    Added article support, added breadcrumbs and minor tweaks

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,4 @@
-export const SITE_TITLE = "My personal website.";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_URL = "https://anyrep.co.uk";
+export const SITE_TITLE = "Anyrep Appliance Repairs";
+export const SITE_DESCRIPTION =
+  "Anyrep offers expert appliance repairs for domestic and commercial appliances in Norwich, Norfolk with over 25 years of experience. Book online or call us today!";
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -31,5 +31,8 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+  ],
 };
